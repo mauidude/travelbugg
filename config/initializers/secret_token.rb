07e188c763ef2c6a2c06f59287c3cf104d95597f
Shane Andrade <shane@Shanes-MacBook-Air.local>
2012-05-31 18:49:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelbugg::Application.config.secret_token = '0954c23a9ba5d2884082dbbb3b11f7c17b8af9f9ba309cc6632b4f6376324f951444e5a2cc9f8e53330677a0540121da756b421fcf9e74e5d15a4c9fdd01583c'
